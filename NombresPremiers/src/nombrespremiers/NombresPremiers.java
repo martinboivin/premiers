e6d0ead244dd49eb1c35f0ef0ad66c1c23be5137
marti <marti@Dell-7420.home>
2024-05-21 08:05:13 -0400

    Ajout d'un println()

--- NombresPremiers/src/nombrespremiers/NombresPremiers.java
+++ NombresPremiers/src/nombrespremiers/NombresPremiers.java
@@ -10,11 +10,11 @@ package nombrespremiers;
 public class NombresPremiers {
 
 	public static void main(String[] args) {
-        int low = 2, high = 50;
+        int low = 20, high = 100;
 
         while (low < high) {
             boolean flag = false;
-            // Um programme par Martin !!! !!!
+            // Um programme par Martin
             for(int i = 2; i <= low/2; ++i) {
                 // Détermine si le nombre est premier
                 if(low % i == 0) {
@@ -28,5 +28,6 @@ public class NombresPremiers {
 
             ++low;
         }             		
+    System.out.println();
 	}
 }
